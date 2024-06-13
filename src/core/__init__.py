f8983b03e7f6bc7fd61d9c2e3173ab88eb98b4e8
猫戸シン <nekotoshin@gmail.com>
2024-06-12 19:48:47 -0700

    chore: update pre-commit config and run pre-commit

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -14,6 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 from .config import Config
 from .database import Database, to_bigint, to_snowflake
 from .logging import InterceptHandler, Logger
--- src/core/database/utils.py
+++ src/core/database/utils.py
@@ -14,6 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 from interactions import Snowflake_Type
 from scyllapy.extra_types import BigInt
 
